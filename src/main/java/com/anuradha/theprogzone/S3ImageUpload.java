560e754ac61a1df05c424fe8b993ce696a9fc3e3
anuradha <anuradha.kadurugasyaya@villvay.com>
2022-05-22 22:42:58 +0530

    first commit

---
+++ src/main/java/com/anuradha/theprogzone/S3ImageUpload.java
@@ -0,0 +1,13 @@
+package com.anuradha.theprogzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ImageUpload {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ImageUpload.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuradha/theprogzone/TheprogzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.theprogzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheprogzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
